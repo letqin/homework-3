6fe1cee44dedd6e81a4c361b54b8d99e127ac091
letqin <letqin@gmail.com>
2021-08-13 10:54:09 -0400

    added in random number generators for all different character types needed

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -11,17 +11,23 @@ function writePassword() {
 }
 
 // lowercase letter generator, chooses 26 numbers from charcode 97
-console.log(Math.floor(Math.random() * 26 ) + 97);
+console.log(Math.floor(Math.random() * 26 ) + 97 );
 // capital letter generator, chooses 26 numbers from charcode 65
-console.log(Math.floor(Math.random() * 26 ) + 65);
-//
+console.log(Math.floor(Math.random() * 26 ) + 65 );
+
+// generates first set of special characters from charcode 33 - 47
+console.log(Math.floor(Math.random() * 15 ) + 33 );
+// generates second set of special characters from charcode 58 - 96
+console.log(Math.floor(Math.random() * 39 ) + 58 );
+
+// generates a random number from charcode 48
+console.log(Math.floor(Math.random() * 10 ) + 48 );
+
+// generates second set of super special characters from charcode 161 - 172
+console.log(Math.floor(Math.random() * 26 ) +  );
+// generates second set of super special characters from charcode 174 - 255
 console.log(Math.floor(Math.random() * 26 ) + 97);
 
-console.log(Math.floor(Math.random() * 26 ) + 97);
-
-console.log(Math.floor(Math.random() * 26 ) + 97);
-
-console.log(Math.floor(Math.random() * 26 ) + 97);
 // generates all possible options for password contents
 function randomLower() {
   // generates lowercase letters
