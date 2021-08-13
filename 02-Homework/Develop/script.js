97e3beedc89e2d65a9a0eda5155b074f5a2a4cc4
letqin <letqin@gmail.com>
2021-08-13 19:56:46 -0400

    wasted my entire day on nothing a day before the due date hooray yippee

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,7 +1,7 @@
-// Assignment Code
+// generates a variable for generateBtn
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// function to create actual password with user selections
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,54 +9,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// lowercase letter generator, chooses 26 numbers from charcode 97
-function lowercase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97 );
-};
-
-// capital letter generator, chooses 26 numbers from charcode 65
-function capital() {
-  return String.fromCharCode(Math.floor(Math.random() * 26 ) + 65 );
-};
-
-// generates a random number from charcode 48
-function number() {
-  return String.fromCharCode(Math.floor(Math.random() * 10 ) + 48 );
-};
-
-// generates first set of special characters from charcode 33 - 47
-function specialOne() {
-  return String.fromCharCode(Math.floor(Math.random() * 15 ) + 33 );
-};
-// generates second set of special characters from charcode 58 - 96
-function specialTwo() {
-  return String.fromCharCode(Math.floor(Math.random() * 39 ) + 58 );
-};
-
-// generates first set of super special characters from charcode 161 - 172
-function superSpecialOne() {
-  return String.fromCharCode(Math.floor(Math.random() * 12 ) + 161 );
-};
-// generates second set of super special characters from charcode 174 - 255
-function superSpecialTwo() {
-  return String.fromCharCode(Math.floor(Math.random() * 82 ) + 174 );
-};
-
-// console logging to check result of functions
-console.log(lowercase());
-
-console.log(capital());
-
-console.log(number());
-
-console.log(specialOne());
-
-console.log(specialTwo());
-
-console.log(superSpecialOne());
-
-console.log(superSpecialTwo());
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
