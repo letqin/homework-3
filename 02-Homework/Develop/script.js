1ee003ff6c1f95389b691c348b880a6dbdf9ed85
letqin <letqin@gmail.com>
2021-08-14 12:18:47 -0400

    added back number based password generation due to time extension

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,6 +1,39 @@
 // generates a variable for generateBtn
 var generateBtn = document.querySelector("#generate");
 
+// // lowercase letter generator, chooses 26 numbers from charcode 97
+// function lowercase() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97 );
+// };
+
+// // capital letter generator, chooses 26 numbers from charcode 65
+// function capital() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26 ) + 65 );
+// };
+
+// // generates a random number from charcode 48
+// function number() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10 ) + 48 );
+// };
+
+// // generates first set of special characters from charcode 33 - 47
+// function specialOne() {
+//   return String.fromCharCode(Math.floor(Math.random() * 15 ) + 33 );
+// };
+// // generates second set of special characters from charcode 58 - 96
+// function specialTwo() {
+//   return String.fromCharCode(Math.floor(Math.random() * 39 ) + 58 );
+// };
+
+// // generates first set of super special characters from charcode 161 - 172
+// function superSpecialOne() {
+//   return String.fromCharCode(Math.floor(Math.random() * 12 ) + 161 );
+// };
+// // generates second set of super special characters from charcode 174 - 255
+// function superSpecialTwo() {
+//   return String.fromCharCode(Math.floor(Math.random() * 82 ) + 174 );
+// };
+
 // function to create actual password with user selections
 const contents = {
   lower = "abcdefghijklmnopqrstuvwxyz",
@@ -13,7 +46,15 @@ function writePassword() {
   var passwordOptions = "";
 };
 
-var length = prompt("How long?")
+var howLong = prompt("How long?");
+
+var wantLower = prompt("Do you want lowercase letters?");
+
+var wantUpper = prompt("Do you want capital letters?");
+
+var wantNum = prompt("Do you want to include numbers?");
+
+var wantSymbol = prompt("Do you want symbols?");
 
 var lower = confirm("Lowercase letters?")
 if (lower) {
