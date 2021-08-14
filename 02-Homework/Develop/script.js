836623685183d5f63781eba6b67326df1d0d4957
letqin <letqin@gmail.com>
2021-08-13 20:35:21 -0400

    forgot some stupid commas. JavaScript gives me a headache

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -3,10 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // function to create actual password with user selections
 const contents = {
-  lower = "abcdefghijklmnopqrstuvwxyz"
-  upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-  number = "0123456789"
+  lower = "abcdefghijklmnopqrstuvwxyz",
+  upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  special = "!#$%&'()*+-./:;<=>?@[]^_`{|}~",
+  number = "0123456789",
 };
 
 function writePassword() {
