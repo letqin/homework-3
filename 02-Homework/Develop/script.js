ca67cbc34fd7836f1241f8a595b5be6ccc16581c
letqin <letqin@gmail.com>
2021-08-18 10:46:52 -0400

    changed the equal signs to colons because that is the right way to do it

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -2,27 +2,26 @@
 var generateBtn = document.querySelector("#generate");
 
 // function to create actual password with user selections
-// const contents = {
-//   lower = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z",
-//   upper = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z",
-//   special = "!,#,$,%,&,',(,),*,+,-,.,/,:,;,<,=,>,?,@,[,],^,_,`,{,|,},~",
-//   number = "0123456789",
-// };
+const contents = {
+  lower: "abcdefghijklmnopqrstuvwxyz",
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  special: "!#$%&'()*+-./:;<=>?@[]^_`{|}~",
+  number: "0123456789"
+};
 
 function writePassword() {
     var passwordOptions = "";
 
   var howLong = prompt("How long?");
 
-  var wantLower = prompt("Do you want lowercase letters?");
+  var wantLower = confirm("Do you want lowercase letters?");
 
-  var wantUpper = prompt("Do you want capital letters?");
+  var wantUpper = confirm("Do you want capital letters?");
 
-  var wantNum = prompt("Do you want to include numbers?");
+  var wantNum = confirm("Do you want to include numbers?");
 
-  var wantSymbol = prompt("Do you want symbols?");
+  var wantSymbol = confirm("Do you want symbols?");
 
-  var lower = confirm("Lowercase letters?")
   if (lower) {
     passwordOptions +- characters.lower
   }
