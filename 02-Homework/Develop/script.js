02613321be56003fbd110785439d9fc9ae086cb1
letqin <letqin@gmail.com>
2021-08-18 11:03:07 -0400

    pushing up changes to go mobile

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -11,21 +11,31 @@ const contents = {
 
 function writePassword() {
     var passwordOptions = "";
-
+  // ask how long to make password
   var howLong = prompt("How long?");
-
+  // ask if there should be lowercase letters
   var wantLower = confirm("Do you want lowercase letters?");
-
-  var wantUpper = confirm("Do you want capital letters?");
-
-  var wantNum = confirm("Do you want to include numbers?");
-
-  var wantSymbol = confirm("Do you want symbols?");
-
+  // ask if there should be capital letters
   if (lower) {
     passwordOptions +- characters.lower
   }
+  var wantUpper = confirm("Do you want capital letters?");
+  // ask if there should be numbers
+  var wantNum = confirm("Do you want to include numbers?");
+  // ask if there should be symbols
+  var wantSymbol = confirm("Do you want symbols?");
+   
 };
+
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
