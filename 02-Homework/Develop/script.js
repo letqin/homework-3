acbabe0bd3a566358f353a5f1776c201ee5dbf8b
letqin <86627655+letqin@users.noreply.github.com>
2021-08-25 14:10:04 -0400

    adding in the function to choose what to include in the password output

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -11,31 +11,39 @@ const contents = {
 
 function writePassword() {
     var passwordOptions = "";
+
   // ask how long to make password
   var howLong = prompt("How long?");
+
   // ask if there should be lowercase letters
   var wantLower = confirm("Do you want lowercase letters?");
+  if (wantLower) {
+    passwordOptions +- contents.lower
+  };
+
   // ask if there should be capital letters
-  if (lower) {
-    passwordOptions +- characters.lower
-  }
   var wantUpper = confirm("Do you want capital letters?");
+  if (wantUpper) {
+    passwordOptions +- characters.upper
+  };
+  
   // ask if there should be numbers
   var wantNum = confirm("Do you want to include numbers?");
+
   // ask if there should be symbols
   var wantSymbol = confirm("Do you want symbols?");
    
 };
 
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
+console.log(howLong);
+console.log(wantLower);
+console.log(wantUpper);
+console.log(wantNum);
+console.log(wantSymbol);
+console.log();
+console.log();
+console.log();
+console.log();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
